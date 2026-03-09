52baec32640f045499155bf57cc1c79acc023aae
mdaburayhanbd <mdaburayhan7475@gmail.com>
2026-03-09 23:34:38 +0600

    add fetchissues

--- dashboard.js
+++ dashboard.js
@@ -13,3 +13,22 @@ const issueCountElement = document.getElementById('issue-count');
 const modal = document.getElementById('issue_detail_modal');
 const modalContent = document.getElementById('modal-dynamic-content');
 const modalBox = modal?.querySelector('.modal-box');
+
+async function fetchIssues() {
+    try {
+        loadingSpinner.style.display = 'flex';
+        issuesGrid.innerHTML = '';
+
+        const response = await fetch('https://phi-lab-server.vercel.app/api/v1/lab/issues');
+        const data = await response.json();
+
+    
+        allIssuesData = Array.isArray(data) ? data : (data.data || []);
+        
+        displayIssues(allIssuesData);
+        loadingSpinner.style.display = 'none';
+    } catch (error) {
+        console.error("Error fetching issues:", error);
+        loadingSpinner.innerHTML = `<p class="text-red-500 font-bold py-10 text-center col-span-full">Failed to load data. Please try again.</p>`;
+    }
+}
